f156399a558c119c026d775ad49cc1185f903524
Helene <hrenninger@crimson.ua.edu>
2025-03-29 14:59:46 -0500

    please add the mvc

---
+++ HL7WebInterface/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HL7WebInterface.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
